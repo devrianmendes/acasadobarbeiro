e083457b6118b4cd348f6413eaaeddd0b6d600bd
devrianmendes <dev.rianmendes@outlook.com>
2024-02-08 11:13:53 -0300

    more changes

--- src/components/Hourse.tsx
+++ src/components/Hourse.tsx
@@ -6,29 +6,46 @@ import bg5 from "../assets/bg6.jpg";
 
 const Hourse = () => {
   return (
-    <section className="bg-mainColor p-10 relative">
-      <div className="flex">
-        <div className="m-1 flex flex-col items-end">
-          <img className="blur-sm mb-1 w-9/12" src={bg1} alt="" />
-          <img className="blur-sm mt-1 w-9/12" src={bg2} alt="" />
+    <section className="bg-mainColor flex justify-center w-full p-4 relative">
+      <div className="max-w-sm sm:max-w-xl lg:max-w-7xl sm:p-10 flex items-center justify-center">
+        <div className="m-1 flex flex-col sm:items-end">
+          <img className="blur-sm mb-1 sm:w-12/12" src={bg1} alt="" />
+          <img className="blur-sm mt-1 sm:w-12/12" src={bg2} alt="" />
         </div>
-        <div className="mr-1 flex justify-start items-center">
-          <img className="blur-sm m-1 w-9/12" src={bg5} alt="" />
+        <div className="hidden mr-1 sm:flex justify-start items-center">
+          <img className="blur-sm m-1 w-12/12" src={bg5} alt="" />
         </div>
       </div>
-      <div className="text-darkWhite border-dotted border-4 rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-10 mb-4 text-center uppercase">
-        <h2 className="text-darkWhite font-titleFont text-2xl pb-4">
-          Horário de funcionamento
-        </h2>
-        <ul className="font-semibold">
-          <li>Domingo - Fechado</li>
-          <li>Segunda - 09 às 17</li>
-          <li>Terça - 09 às 17</li>
-          <li>Quarta - 09 às 17</li>
-          <li>Quinta - 09 às 17</li>
-          <li>Sexta - 09 às 17</li>
-          <li>Sábado - 09 às 17</li>
-        </ul>
+      <div className="max-w-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="text-darkWhite p-10 mb-10 text-center uppercase border-dotted border-4 rounded-xl">
+          <h2 className="text-darkWhite font-titleFont text-2xl pb-4">
+            Horário de funcionamento
+          </h2>
+          <ul className="font-semibold">
+            <li>Domingo - Fechado</li>
+            <li>Segunda - 09 às 17</li>
+            <li>Terça - 09 às 17</li>
+            <li>Quarta - 09 às 17</li>
+            <li>Quinta - 09 às 17</li>
+            <li>Sexta - 09 às 17</li>
+            <li>Sábado - 09 às 17</li>
+          </ul>
+        </div>
+        <div className="hidden lg:block text-darkWhite">
+          <p className="font-bold mb-4">
+            Nosso ambiente foi montado priorizando o conforto e qualidade de
+            atendimento:
+          </p>
+          <ul>
+            <li>✓ Ambiente sempre acolhedor</li>
+            <li>✓ Nossos profissionais prezam por qualidade</li>
+            <li>✓ Valorizamos o tempo e o dinheiro dos nossos clientes</li>
+            <li>✓ Trabalhamos com produtos de alta qualidade</li>
+            <li>
+              ✓ Todos os equipamentos são desinfetados antes e depois do uso
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
     // <section className=" bg-manCuttingHair py-14 px-yyar6 flex justify-center w-full h-auto bg-center bg-no-repeat">
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,72 +11,73 @@ import { FaWhatsapp } from "react-icons/fa";
 const Intro = () => {
   return (
     <section className="py-14 px-6">
-      <div>
-
-      <div className="flex justify-center">
-        <div className="max-w-sm md:max-w-6xl md:flex">
-          <div className="pb-4 md:w-6/12 flex flex-col justify-center">
-            <div className="pb-4 md:text-xl">
-              <div className="my-3">
-                <p>Bem-vindos </p>
+      <div className="flex flex-col">
+        <div className="flex justify-center mb-10">
+          <div className="max-w-sm md:max-w-6xl md:flex">
+            <div className="pb-4 md:w-6/12 flex flex-col justify-center">
+              <div className="pb-4 md:text-xl">
+                <div className="my-3">
+                  <p>Bem-vindos </p>
+                </div>
+                <div>
+                  <h1 className="font-titleFont my-3 text-2xl md:text-4xl">
+                    A CASA DO BARBEIRO
+                  </h1>
+                </div>
+                <div className="flex items-center my-3">
+                  <FaMapMarkerAlt />
+                  <p>Rua Fictícia, 130</p>
+                  <p>Centro, Rio de Janeiro</p>
+                </div>
+                <div className="flex items-center my-3">
+                  <BsFillTelephoneFill />
+                  <p>(21)2222-2222</p>
+                </div>
+                <div className="flex items-center my-3">
+                  <FaWhatsapp />
+                  <p>(21)99999-9999</p>
+                </div>
               </div>
-              <div>
-                <h1 className="font-titleFont my-3 text-2xl md:text-4xl">
-                  A CASA DO BARBEIRO
-                </h1>
-              </div>
-              <div className="flex items-center my-3">
-                <FaMapMarkerAlt />
-                <p>Rua Fictícia, 130</p>
-                <p>Centro, Rio de Janeiro</p>
-              </div>
-              <div className="flex items-center my-3">
-                <BsFillTelephoneFill />
-                <p>(21)2222-2222</p>
-              </div>
-              <div className="flex items-center my-3">
-                <FaWhatsapp />
-                <p>(21)99999-9999</p>
+              <button className="font-bold max-w-64 border-2 rounded-lg py-3 px-5 uppercase flex items-center justify-center hover:bg-mainColor hover:border-mainColor hover:text-darkWhite ease-in duration-100">
+                <FaWhatsapp className="mr-2" />
+                Agende um horário
+              </button>
+            </div>
+            <div className="md:w-6/12">
+              <div className="pb-6">
+                <img
+                  src={intro1}
+                  className="rounded-lg"
+                  alt="Foto de um homem com cabelo e barba feitos"
+                />
               </div>
             </div>
-            <button className="font-bold max-w-64 border-2 rounded-lg py-3 px-5 uppercase flex items-center justify-center hover:bg-mainColor hover:border-mainColor hover:text-darkWhite ease-in duration-100">
-              <FaWhatsapp className="mr-2" />
-              Agende um horário
-            </button>
           </div>
-          <div className="md:w-6/12">
-            <div className="pb-6">
+        </div>
+        <div className="flex justify-center">
+          <div className="max-w-sm md:max-w-6xl grid grid-cols-2 md:grid-cols-4 font-bold text-2xl text-center">
+            <div className="flex flex-col items-center pb-4">
+              <img className="w-20" src={beard} alt="icon of a beard" />
+              <p>Cuidados faciais</p>
+            </div>
+            <div className="flex flex-col items-center pb-4">
+              <img className="w-20" src={scissor} alt="Ícone de uma tesoura" />
+              <p>Corte de cabelo</p>
+            </div>
+            <div className="flex flex-col items-center">
               <img
-                src={intro1}
-                className="rounded-lg"
-                alt="Foto de um homem com cabelo e barba feitos"
+                className="w-20"
+                src={machine}
+                alt="Ícone de uma máquina de cortar cabelo"
               />
+              <p>Barba</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <img className="w-20" src={kid} alt="Ícone de uma criança" />
+              <p>Corte de cabelo infantil</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="max-w-sm align-middle grid grid-cols-2 text-center">
-        <div className="flex flex-col items-center pb-4">
-          <img className="w-20" src={beard} alt="icon of a beard" />
-          <p>Cuidados faciais</p>
-        </div>
-        <div className="flex flex-col items-center pb-4">
-          <img className="w-20" src={scissor} alt="Ícone de uma tesoura" />
-          <p>Corte de cabelo</p>
-        </div>
-        <div className="flex flex-col items-center">
-          <img
-            className="w-20"
-            src={machine}
-            alt="Ícone de uma máquina de cortar cabelo"
-          />
-          <p>Barba</p>
-        </div>
-        <div className="flex flex-col items-center">
-          <img className="w-20" src={kid} alt="Ícone de uma criança" />
-          <p>Corte de cabelo infantil</p>
-        </div>
-      </div>
       </div>
     </section>
   );
